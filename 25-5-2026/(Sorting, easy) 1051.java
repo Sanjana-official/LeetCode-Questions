2bcdcca8b77b8f9a56140967a1668601f744c6c9
Sanjana <sanjanaofficial879@gmail.com>
2026-05-25 21:48:15 +0530

    Create (Sorting, easy) 1051.java

---
+++ 25-5-2026/(Sorting, easy) 1051.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] sorted = new int[heights.length];
+        for(int i =0; i < heights.length; i++) {
+            sorted[i] = heights[i];
+        }
+        Arrays.sort(sorted);
+
+        int total = 0;
+        for(int i =0; i < heights.length; i++) {
+            if(sorted[i] != heights[i]) total++;
+        }
+
+        return total;
+    }
+}
