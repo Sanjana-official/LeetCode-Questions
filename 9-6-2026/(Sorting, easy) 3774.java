34b47926ff55e678bf43bad85d2fbf1afd4c6912
Sanjana <sanjanaofficial879@gmail.com>
2026-06-09 22:20:43 +0530

    Create (Sorting, easy) 3774.java

---
+++ 9-6-2026/(Sorting, easy) 3774.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int absDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int maxSum = 0;
+        int minSum = 0;
+        for(int i = 0; i < k; i++){
+            minSum += nums[i];
+        }
+        for(int i = nums.length - 1; i >= nums.length - k; i--){
+            maxSum += nums[i];
+        }
+        return Math.abs(maxSum - minSum);
+    }
+}
