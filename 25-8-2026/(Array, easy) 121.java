c4954f7fd73f33968ae441999ed55049cfb78d35
Sanjana <sanjanaofficial879@gmail.com>
2026-08-25 21:08:54 +0530

    Create (Array, easy) 121.java

---
+++ 25-8-2026/(Array, easy) 121.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy= Integer.MAX_VALUE;
+        int maxProfit = 0;
+        int profit=0;
+        for(int i=0;i<prices.length;i++)
+        {
+            if(buy<prices[i])
+            {
+                profit = prices[i]-buy;
+                maxProfit = Math.max(maxProfit,profit);
+            }else
+            {
+                buy=prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
